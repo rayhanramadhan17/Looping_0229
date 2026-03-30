db23b64c880be21c4baf95e07b63755b9b2c8ecb
rayhanramadhan17 <rayhanramadhan179072@gmail.com>
2026-03-30 19:13:58 +0800

    looping 1

--- looping.cpp
+++ looping.cpp
@@ -11,32 +11,3 @@ int main() {
         cout << "Teknologi Informasi UMY" << endl;
     }
     cout << endl;
-
-    // Random angka
-    srand(time(0));
-
-    // Perulangan WHILE
-    cout << "PERULANGAN WHILE" << endl;
-    x = 1 + rand() % 10;
-
-    while (x <= 5) {
-        cout << "Bilangan acak = " << x << endl;
-        x = rand() % 10;
-    }
-
-    cout << "Bilangan acak while yang terakhir = " << x << endl;
-    cout << endl;
-
-    // Perulangan DO-WHILE
-    cout << "PERULANGAN DO-WHILE" << endl;
-    x = 1 + rand() % 10;
-
-    do {
-        cout << "Bilangan acak = " << x << endl;
-        x = rand() % 10;
-    } while (x <= 5);
-
-    cout << "Bilangan acak do-while yang terakhir = " << x << endl;
-
-    return 0;
-}
