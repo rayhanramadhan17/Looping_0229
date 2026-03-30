cec1867b598e3a9338d3d3b3b66c57af670a657e
rayhanramadhan17 <rayhanramadhan179072@gmail.com>
2026-03-30 19:13:34 +0800

    first commit

---
+++ looping.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main() {
+    int x;
+
+    // Perulangan FOR
+    cout << "PERULANGAN FOR" << endl;
+    for (int i = 0; i <= 4; i++) {
+        cout << "Teknologi Informasi UMY" << endl;
+    }
+    cout << endl;
+
+    // Random angka
+    srand(time(0));
+
+    // Perulangan WHILE
+    cout << "PERULANGAN WHILE" << endl;
+    x = 1 + rand() % 10;
+
+    while (x <= 5) {
+        cout << "Bilangan acak = " << x << endl;
+        x = rand() % 10;
+    }
+
+    cout << "Bilangan acak while yang terakhir = " << x << endl;
+    cout << endl;
+
+    // Perulangan DO-WHILE
+    cout << "PERULANGAN DO-WHILE" << endl;
+    x = 1 + rand() % 10;
+
+    do {
+        cout << "Bilangan acak = " << x << endl;
+        x = rand() % 10;
+    } while (x <= 5);
+
+    cout << "Bilangan acak do-while yang terakhir = " << x << endl;
+
+    return 0;
+}
