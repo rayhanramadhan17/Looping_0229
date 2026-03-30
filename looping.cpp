60410e17504a9f429854c0b9963e71b8b251f5d0
rayhanramadhan17 <rayhanramadhan179072@gmail.com>
2026-03-30 19:15:39 +0800

    looping 3

--- looping.cpp
+++ looping.cpp
@@ -27,3 +27,16 @@ int main() {
     cout << "Bilangan acak while yang terakhir = " << x << endl;
     cout << endl;
 
+    // Perulangan DO-WHILE
+    cout << "PERULANGAN DO-WHILE" << endl;
+    x = 1 + rand() % 10;
+
+    do {
+        cout << "Bilangan acak = " << x << endl;
+        x = rand() % 10;
+    } while (x <= 5);
+
+    cout << "Bilangan acak do-while yang terakhir = " << x << endl;
+
+    return 0;
+}
